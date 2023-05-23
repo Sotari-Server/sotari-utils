7c857ecb035a06d02bc44d9a072034e251fc22ee
Sotari Server <134143153+Sotari-Server@users.noreply.github.com>
2023-05-23 16:05:49 +0200

    Delete SotariMinecraftLauncher directory

--- SotariMinecraftLauncher/src/main/java/fr/knsrhuseyin/utils/json_reader/JSONStreamAware.java
+++
@@ -1,8 +0,0 @@
-package fr.knsrhuseyin.utils.json_reader;
-
-import java.io.IOException;
-import java.io.Writer;
-
-public interface JSONStreamAware {
-    void writeJSONString(Writer out) throws IOException;
-}
